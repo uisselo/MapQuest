2b66c9d93a42155fab0771c1df5300f922ab8067
Lorraine Evale <louisse.evale.iics@ust.edu.ph>
2021-10-18 20:16:55 +0800

    fixed minor errors

--- mapquest_parse-json_1.py
+++ mapquest_parse-json_1.py
@@ -12,6 +12,7 @@ def convert(miles, newUnit):
         return miles
 
 while True:
+    # user inputs
     orig = input("Starting location: ")
     if orig == "quit" or orig == "q":
         break
@@ -24,17 +25,17 @@ while True:
 
     url = main_api + urllib.parse.urlencode({"key": key, "from": orig, "to": dest})
     json_data = requests.get(url).json()
-    print("URL: " (url))
+    print("URL: " + (url))
     json_data = requests.get(url).json()
-    json_status = json_data["info"]["status code"]
+    json_status = json_data["info"]["statuscode"]
     
     if json_status == 0:
         print("API Status: " + str(json_status) + " = A successful route call.\n")
         print("=============================================")
         print("Directions from: " + (orig) + " to " + (dest))
         print("Trip Duration: " + (json_data["route"]["formattedTime"]))
-        print("Kilometers: " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
-        print("Fuel used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
+        print(units + ": " + str("{:.2f}".format(convert(json_data["route"]["distance"], units))))
+        print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
         print("=============================================")
         for each in json_data["route"]["legs"][0]["maneuvers"]:
             print((each["narrative"]) + " (" + str("{:.2f}".format(convert(each["distance"], units)) + " " + units.lower() + ")"))
